12b8a73d179e35876caa88642cd869e13ad3cbdc
Dave Stillman <david.stillman@sparkfun.com>
2014-10-28 09:42:11 -0600

    add flex

---
+++ flex/flex.ino
@@ -0,0 +1,43 @@
+// flex resistor example
+#include <SoftwareSerial.h>
+
+SoftwareSerial display(3, 2);
+
+char flexstring[10];
+int flex, degrees;
+
+void setup()
+{
+  Serial.begin(9600);  // Start serial for output
+
+  display.begin(9600);
+  Serial.begin(9600);
+  delay(500);
+
+  display.write(254); // move cursor to beginning of first line
+  display.write(128);
+
+  display.write("                "); // clear display
+  display.write("                ");
+}
+
+void loop()
+{
+  flex = analogRead(0);
+
+  degrees = map(flex, 650, 853, 0, 90);
+
+  Serial.print(flex);
+  Serial.print(" ");
+  Serial.println(degrees);
+  sprintf(flexstring, "%3d", degrees);
+
+  display.write("flex: ");
+  display.write(flexstring);
+
+  display.write(254); // move cursor to beginning of first line
+  display.write(128);
+
+  delay(100);
+}
+
